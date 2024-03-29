cbbd044b60c7a816174b13b581191e706a52a28f
jujobs <147573503+jujobs@users.noreply.github.com>
2024-03-29 16:43:28 -0300

    Add files via upload

---
+++ augustos/js/script.js
@@ -0,0 +1,65 @@
+function fname(){
+    let comida = document.getElementById('conteudocomida1')
+    comida.style = "height:300px;"
+    let butt = document.get
+}
+
+function fname2(){
+    let comida = document.getElementById('conteudocomida1')
+    comida.style = "height:35px;"
+}
+
+function fname3(){
+    let comida = document.getElementById('conteudocomida2')
+    comida.style = "height:300px;"
+    let butt = document.get
+}
+
+function fname4(){
+    let comida = document.getElementById('conteudocomida2')
+    comida.style = "height:35px;"
+}
+
+function fname5(){
+    let comida = document.getElementById('conteudocomida3')
+    comida.style = "height:300px;"
+    let butt = document.get
+}
+
+function fname6(){
+    let comida = document.getElementById('conteudocomida3')
+    comida.style = "height:35px;"
+}
+
+function fname7(){
+    let comida = document.getElementById('conteudocomida4')
+    comida.style = "height:300px;"
+    let butt = document.get
+}
+
+function fname8(){
+    let comida = document.getElementById('conteudocomida4')
+    comida.style = "height:35px;"
+}
+
+function fname9(){
+    let comida = document.getElementById('conteudocomida5')
+    comida.style = "height:300px;"
+    let butt = document.get
+}
+
+function fname10(){
+    let comida = document.getElementById('conteudocomida5')
+    comida.style = "height:35px;"
+}
+
+function fname11(){
+    let comida = document.getElementById('conteudocomida6')
+    comida.style = "height:300px;"
+    let butt = document.get
+}
+
+function fname12(){
+    let comida = document.getElementById('conteudocomida6')
+    comida.style = "height:35px;"
+}
